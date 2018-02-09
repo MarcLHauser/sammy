d46c7dd6525c0fade5b965cde69b65b8161948ad
Marc Hauser <mhauser@ocean7.com>
2018-02-09 12:08:31 -0500

    simplify. add all data to events

--- game.js
+++ game.js
@@ -11,116 +11,79 @@ var event = {
     },
     
     "stay in hotel?": {
-        question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?"
+        question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?",
+        yes: "go to Chicago?",
+        no: "alleyway"
+    },
+    
+    "alleyway": {
+        causeDeath: "You decide to sleep in an abandoned alleyway and find all your money gone in the morning. You starve to death because no one will give you a job."
     },
     
     "go to Chicago?": {
-        question: "The man makes you pay enormous fees to get out. Once you get out of the hotel, you remember that your friend got rich in Chicago. Do you want to go to Chicago?"
+        question: "The man makes you pay enormous fees to get out. Once you get out of the hotel, you remember that your friend got rich in Chicago. Do you want to go to Chicago?",
+        yes:  "go to stockyards?",
+        no:  "homeless" 
     },
     
     "stay in New York tenement?": {
-        question: "You stay in New York. Do you want to stay in a tenement?"
+        question: "You stay in New York. Do you want to stay in a tenement?",
+        no: "homeless",
+        yes: "tenement"
+    },
+    
+    "tenenment": {
+        causeDeath: "You get tuberculosis after a few days in the tenement. It is fatal."
+    },
+    
+    "homeless": {
+        causeDeath: "You are homeless and you can't find work. You starve."
     },
     
     "go to stockyards?": {
-        question: "You finally arrive in Chicago and you learn a new word: 'stockyards'. Do you want to go to the stockyards?"
+        question: "You finally arrive in Chicago and you learn a new word: 'stockyards'. Do you want to go to the stockyards?",
+        no: "wandering chicago",
+        yes: "boardinghouse in stockyards?"
+    },
+    
+    "wandering chicago" : {
+        causeDeath : "You starve wandering Chicago."
     },
     
     "boardinghouse in stockyards?": {
-        question: "You arrive in the stockyards. Coincidencetally, you run into your friend who owns a bodega. He gives a recommendation for a boardinghouse. Do you stay there?"
+        question: "You arrive in the stockyards. Coincidencetally, you run into your friend who owns a bodega. He gives a recommendation for a boardinghouse. Do you stay there?",
+        no: "chicago tenement"
+    },
+    
+    "chicago tenement" : {
+        causeDeath:  "You quickly realize that the whole family cannot sleep in the streets. You find a tenement building to stay in for very little money, but it is very small and cramped. You get sick, because of the cramped consitions, and the disease is fatal."
     }
     
 };
 
-var player = {
-    alive: true,
-    causeDeath: "yay",
-    
-    // define the player's initial life value to an event object
-    life: event['go to America?'],
-
-};
+var state = 'go to America?';
 
 var ask = function(){
     
-    if( player.life.causeDeath === undefined ){
+    if( event[state].causeDeath === undefined ){
         
-        console.log( player.life.question );
-        return "";
-    
+        return event[state].question;
+        
     } else {
-    
-        console.log( player.life.causeDeath );
+        
+        console.log( event[state].causeDeath );
         return "Game Over.";
     }
-
 };
 
-var n = 0;  
-
 var yes = function(){
-    
-    player.life = event[ player.life.yes ];
+    state = event[state].yes;
     return ask();
-    
-    //go to stockyards?
-    if (n == 4){
-        player.questionNumber = questions[5];
-        return ask();
-    }
-    
-    //stay in New York tenement?
-    if (n == 3){
-        player.alive = false;
-        player.causeDeath = "You get tuberculosis after a few days in the tenement. It is fatal.";
-    }
 };
 
 var no = function(){
-    
-    player.life = event[ player.life.no ];
+    state = event[state].no;
     return ask();
-    
-    //go to America?
-    if(n === 0){
-    player.alive = false;
-    player.causeDeath = "You have frozen to death because of Lithuania's cold climate.";
-    return ask();
-    }
-    
-    //stay in hotel?
-    if(n == 1){
-        player.alive = false;
-        player.causeDeath = "You decide to sleep in an abandoned alleyway and find all your money gone in the morning. You starve to death because no one will give you a job.";
-        return ask();
-    }
-    
-    //go to Chicago?
-    if (n == 2){
-        player.questionNumber = questions[3];
-        return ask();
-    }
-    
-    //stay in tenement in New York?
-    if (n == 3){
-        player.alive = false;
-        player.causeDeath = "You are homeless and you can't find work. You starve.";
-        return ask();
-    }
-    
-    //go to stockyards?
-    if (n == 4){
-        player.alive = false;
-        player.causeDeath = "You starve wandering Chicago.";
-        return ask();
-    }
-    
-    //boardinghouse in stockyards?
-    if (n == 5){
-        player.alive = false;
-        player.causeDeath = "You quickly realize that the whole family cannot sleep in the streets. You find a tenement building to stay in for very little money, but it is very small and cramped. You get sick, because of the cramped consitions, and the disease is fatal.";
-        return ask();
-    }
 };  
 
-ask();
+console.log( ask() );
